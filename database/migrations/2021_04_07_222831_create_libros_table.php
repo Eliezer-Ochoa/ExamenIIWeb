f0ccdc640d05489ec8db1464085756d46a503e3b
Eliezer Ochoa <eliezerochoa20@gmail.com>
2021-04-08 01:03:32 +0200

    Agregue el controller cree la tabla libro y la migre a la base de datos examen

---
+++ database/migrations/2021_04_07_222831_create_libros_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLibrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('libros', function (Blueprint $table) {
+            $table->id();
+            $table->string('isbn',10);
+            $table->string('titulo',50);
+            $table->string('editorial');
+            $table->string('autor',50);
+            $table->string('edicion',10);
+            $table->date('fecha',50);
+           
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('libros');
+    }
+}
